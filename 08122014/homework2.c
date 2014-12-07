35389423348efedcab2f741519c370788fc16a66
SerjZi <serejka.minsk@mail.ru>
2014-12-08 00:17:57 +0300

    homework2

---
+++ 08122014/homework2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+	int i, n;
+	
+	printf("enter number: ");
+	scanf("%d",&n);
+	for(i=1; i<=n; i++)
+	{
+		printf("%d\n", i);
+	}
+	return 0;
+}
